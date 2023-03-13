8d235244ec598e00a3cde5c8ff3123ef43202251
njiddasalifu <njiddasalifu@gmail.com>
2023-03-13 12:05:46 +0100

    Login and logout buttons working correctly

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -15,19 +15,23 @@ const Login = details => {
 
   if(details.email==adminUser.email && details.password == adminUser.password) {
   console.log("Logged in");
+  setUser({
+    name: details.name,
+    email: details.email
+  });
   }else {
     console.log("Details do not match");
   }
 }
 const Logout =()=> {
-  console.log("Logout");
+ setUser({name:"", email: ""});
 }
   return (
     <div className="App">
      {(user.email!= "") ? (
       <div className='Welcome'>
         <h2>Welcome, <span>{user.name}</span></h2>
-        <button>Logout</button> 
+        <button onClick={Logout}>Logout</button> 
         </div>
      ): (
       <Loginform Login={Login} error={error}/>
