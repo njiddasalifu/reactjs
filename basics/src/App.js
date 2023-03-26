ebb42cd53789a01c3f0a17c57e385b63a3611f98
njiddasalifu <njiddasalifu@gmail.com>
2023-03-26 07:42:40 +0100

    using useState hook to change role with input from the input field

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
+
 
 function App(){
+  // role and setRole are just variables
+  const [role, setRole] = useState();
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? // this line means if its true
     <>
+    <input 
+    type='text' 
+    onChange={(e)=>{
+      setRole(e.target.value);
+    }}
+    />
     
-    <Employee name="Salifu" role="IT Specialist" />
+    <Employee name="Salifu" role={role} />
+    
     </>
 // this line below means if its false
    : <p>You can not see the employees.</p>
