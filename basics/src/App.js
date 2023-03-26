86f01fc15d0c7d22eeaaf0cde12bb044620c61a7
njiddasalifu <njiddasalifu@gmail.com>
2023-03-26 07:06:06 +0100

    using propd to give attributes like name to the employee

--- basics/src/App.js
+++ basics/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import Employee from './components/Employee';
 
 function App(){
-  const showEmployees = false;
+  const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? // this line means if its true
     <>
     
-    <Employee />
+    <Employee name="Salifu" />
     </>
 // this line below means if its false
    : <p>You can not see the employees.</p>
--- basics/src/components/Employee.js
+++ basics/src/components/Employee.js
@@ -1,7 +1,7 @@
 
-function Employee(){ // Note: each componrnt starts with an uppercase letter
+function Employee(props){ // Note: each componrnt starts with an uppercase letter
     //else it will not work
-return <h3>Here is an employeee</h3>;
+return <h3>Here is an employeee by name: {props.name}</h3>;
 }
 
 export default Employee;
