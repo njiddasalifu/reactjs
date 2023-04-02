a71c43ce30b8a58d87651cfca01c701ca7ef5fb6
njiddasalifu <njiddasalifu@gmail.com>
2023-04-02 02:04:49 +0100

    adding more features as learning continues

--- basics/src/components/Employee.js
+++ basics/src/components/Employee.js
@@ -2,11 +2,22 @@
 function Employee(props){ // Note: each componrnt starts with an uppercase letter
     //else it will not work
 return (
-<>
-<p><b>Here is an employeee by name:</b> {props.name}</p>
-<p><p><b>Role:</b> </p>{props.role ? props.role : 'No role'}</p>
+<div>
+    <img 
+    class="block mx-auto h-24 rounded-full sm:shrink-0"
+    src="/img/origin-lindford.jpg"
+    alt="Woman's face" />
+    <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+            <p class="text-lg text-black font-semibold">{props.name}</p>
+            <p class="text-state-500 font-medium">{props.role}</p>
+        </div>
+        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border">
+            message
+        </button>
 
-</>
+    </div>
+</div>
 );
 }
 
