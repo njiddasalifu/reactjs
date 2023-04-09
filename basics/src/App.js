a8c63e0e2b1941d7d4ae1bbc7acc136b7761906f
njiddasalifu <njiddasalifu@gmail.com>
2023-04-09 12:13:55 +0100

    app.js mode codes

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,32 +1,16 @@
-import './index.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
-
-
-function App(){
-  // role and setRole are just variables
-  const [role, setRole] = useState();
-  const showEmployees = true;
-  return (
-    <div className="App bg-red-300">
-      {showEmployees ? // this line means if its true
-    <>
-    <input 
-    type='text' 
-    onChange={(e)=>{
-      setRole(e.target.value);
-    }}
-    />
-  
-    <Employee name="Salifu" role={role} />
-    
-    </>
-// this line below means if its false
-   : <p>You can not see the employees.</p>
-      }  
-    </div>
+function MyButton(){
+  return(
+    <button>
+      I'm a button
+    </button>
   );
-
 }
 
-export default App;
+export default function MyApp(){
+  return(
+    <div>
+    <h1>Welcome to my app</h1>
+    <MyButton />
+    </div>
+  );
+}
