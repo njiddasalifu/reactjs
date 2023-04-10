9830d2434433a6d9fb4919446e46e23ba21fa8cf
njiddasalifu <njiddasalifu@gmail.com>
2023-04-10 01:00:58 +0100

    working with components

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Hello from './componets/hello';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyApp />
+    <Hello />
   </React.StrictMode>
 );
 
