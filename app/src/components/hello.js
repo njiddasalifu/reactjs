da3f1d0270688d696323725d4c3e30f4fe48304b
njiddasalifu <njiddasalifu@gmail.com>
2023-04-12 17:02:02 +0100

    displaying fav fruits using JSX

--- app/src/components/hello.js
+++ app/src/components/hello.js
@@ -1,15 +1,15 @@
 function Hello(){
     return(
-      <>
-      <p><h2>My Favorite fruits</h2></p>
-      <ul>
-        <li>Apples</li>
-        <li>Bananas</li>
-        <li>Mangoes</li>
-        <li>Pears</li>
-      </ul>
-
-      </>
+        <>
+        <p><h2>My Favorite fruits</h2></p>
+        <ul>
+          <li>Apples</li>
+          <li>Bananas</li>
+          <li>Mangoes</li>
+          <li>Pears</li>
+        </ul>
+  
+        </>
     );
 
 }
