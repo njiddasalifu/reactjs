dee6a7544dc41bc441034bb44384285b74bcc013
njiddasalifu <njiddasalifu@gmail.com>
2023-04-12 17:07:25 +0100

    executing ternary operators directly in index.js

--- app/src/index.js
+++ app/src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Hello from './components/hello';
 
+const x =50;
+const myElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <Hello />
-  </React.StrictMode>
+  myElement
 );
 
 // If you want to start measuring performance in your app, pass a function
