ff486742d67e51bc0bcc2aa864b7d40bcb08400a
njiddasalifu <njiddasalifu@gmail.com>
2023-04-17 22:14:39 +0100

    inline style to components

--- app/src/index.js
+++ app/src/index.js
@@ -10,6 +10,8 @@ root.render(
    <Hello />
    <Expense />
    <Car color= "red" name= "Benz" />
+   <Car color= "red" name= "Benz" />
+   <Car color= "red" name= "Benz" />
 
   </React.StrictMode>
 );
