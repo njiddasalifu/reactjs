c66a00dd1df2d40ce58710375a2a512ca11380b1
njiddasalifu <njiddasalifu@gmail.com>
2023-04-18 05:07:14 +0100

    lists using a key

--- app/src/components/list.js
+++ app/src/components/list.js
@@ -3,12 +3,17 @@ function Car(props){
 }
 
 function Garrage(){
-    const cars = ['Ford', 'BMW', 'Audi']
+    const cars = [
+        // a key allow react to keep track of elements
+        {id:1, brand: 'Ford'},
+        {id:2, brand: 'BMW'},
+        {id:3 , brand: 'Audi'}
+    ];
     return(
         <>
         <h1>Who lives in my Garrage?</h1>
         <ul>
-            {cars.map((car)=><Car brand={car} />)}
+            {cars.map((car)=><Car key={car.id} brand={car.brand} />)}
         </ul>
         </>
     );
