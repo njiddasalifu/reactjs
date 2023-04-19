58d611664596d68a8f7d7e0efe17ae8eae8bbd86
njiddasalifu <njiddasalifu@gmail.com>
2023-04-19 10:27:35 +0100

    app.js no code

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import hello from './components/hello';
 import React from 'react';
 import { ReactDOM } from 'react';
-const x = 5;
-
-const myElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
